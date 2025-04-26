3bb0ade12ec4636c21f008bc48843fc98354f88c
James Yuill <jamesyuill@macbookpro.lan>
2025-04-26 20:49:29 +0100

    updates with smaller images for the smaller preview cards

--- experiments/experiment-slugs.js
+++ experiments/experiment-slugs.js
@@ -1,52 +1,52 @@
 const experimentSlugs = [
   {
     title: 'Transparent Browser Window',
-    img: '../assets/experiment-images/transparent-browser-desktop.png',
+    img: '../assets/experiment-images-small/transparent-browser-small.png',
     about: 'Idea to make a browser that was transparent',
     href: '../experiments/transparent-browser.html',
   },
   {
     title: 'Browser Battleships',
-    img: '../assets/experiment-images/browser-battleships-desktop.png',
+    img: '../assets/experiment-images-small/browser-battleships-small.png',
     about:
       'A game of battleships, but you have to place little browsers around the screen',
     href: '../experiments/browser-battleships.html',
   },
   {
     title: 'Browser Pong',
-    img: '../assets/experiment-images/browser-pong-desktop.png',
+    img: '../assets/experiment-images-small/browser-pong-small.png',
     about:
       'A game of Pong, but using browser windows (I got very into messing about with the browser!)',
     href: '../experiments/browser-pong.html',
   },
   {
     title: 'Cli Art',
-    img: '../assets/experiment-images/cli-art-desktop.png',
+    img: '../assets/experiment-images-small/cli-art-small.png',
     about:
       'Random CLI are generator, but in three languages, Javascript, C++ and Rust',
     href: '../experiments/cli-art.html',
   },
   {
     title: 'Audio Rendering',
-    img: '../assets/experiment-images/audio-rendering-desktop.png',
+    img: '../assets/experiment-images-small/audio-rendering-small.png',
     about: 'Unlock a website by singing a certain pitch',
     href: '../experiments/audio-rendering.html',
   },
   {
     title: 'Perfect Pitcher',
-    img: '../assets/experiment-images/perfect-pitcher-desktop.png',
+    img: '../assets/experiment-images-small/perfect-pitcher-small.png',
     about: 'Test your singing skills with my perfect pitch tester!',
     href: '../experiments/perfect-pitcher.html',
   },
   {
     title: 'Wiki Picture Scrapper',
-    img: '../assets/experiment-images/wiki-webscraper-desktop.png',
+    img: '../assets/experiment-images-small/wiki-webscraper-small.png',
     about: 'Webscrapper that grabs the pictures embedded in a Wiki page',
     href: '../experiments/wiki-scraper.html',
   },
   {
     title: '3d Snake',
-    img: '../assets/experiment-images/3d-snake-desktop.png',
+    img: '../assets/experiment-images-small/3d-snake-small.png',
     about: 'Snake, but 3D!',
     href: '../experiments/3d-snake.html',
   },
--- projects/project-slugs.js
+++ projects/project-slugs.js
@@ -1,70 +1,70 @@
 const projectSlugs = [
   {
     title: 'Supplier Satellites',
-    img: '../assets/project-images/supplier-satellites-desktop.png',
+    img: '../assets/project-images-small/supplier-satellites-small.png',
     about: 'Custom Power BI visual for supplier management',
     href: '../projects/supplier-satellites.html',
   },
   {
     title: 'React Multiplayer Quiz Platform',
-    img: '../assets/project-images/multiplayer-quiz-desktop.png',
+    img: '../assets/project-images-small/multiplayer-quiz-small.png',
     about: 'React Multiplayer quiz platform utilising socket.io',
     href: '../projects/multiplayer-quiz.html',
   },
   {
     title: 'Standup Pickeroo',
-    img: '../assets/project-images/standup-pickeroo-desktop.png',
+    img: '../assets/project-images-small/standup-pickeroo-small.png',
     about: 'Spin the Wheel style person picker for standups',
     href: '../projects/standup-pickeroo.html',
   },
   {
     title: 'Portfolio 2024',
-    img: '../assets/project-images/jy-portfolio-2024.png',
+    img: '../assets/project-images-small/jy-portfolio-2024-small.png',
     about: 'My 2024 portfolio website',
     href: '../projects/portfolio-2024.html',
   },
   {
     title: 'Project To-Do',
-    img: '../assets/project-images/project-todo-desktop.png',
+    img: '../assets/project-images-small/project-todo-small.png',
     about: 'A Kanban style app...maybe like Trello...shhh!',
     href: '../projects/project-todo.html',
   },
   {
     title: 'Balls Dropping',
-    img: '../assets/project-images/balls-dropping-desktop.png',
+    img: '../assets/project-images-small/balls-dropping-small.png',
     about: 'A synth where the notes are triggered by balls dropping',
     href: '../projects/balls-dropping.html',
   },
   {
     title: 'cable tv landing Page',
-    img: '../assets/project-images/cable-tv-website.png',
+    img: '../assets/project-images-small/cable-tv-website-small.png',
     about: 'A simple landing page for a musical artist',
     href: '../projects/cable-tv.html',
   },
   {
     title: 'Etsy to Instagram Scraper',
-    img: '../assets/project-images/etsy-insta-scraper-big.png',
+    img: '../assets/project-images-small/etsy-insta-scraper-small.png',
     about:
       'Web scrapper that will grab contents of Etsy shop and selects random item to post about on Instagram',
     href: '../projects/etsy-to-insta.html',
   },
   {
     title: 'Guess What?!',
-    img: '../assets/project-images/guess-what-desktop.png',
+    img: '../assets/project-images-small/guess-what-small.png',
     about:
       'Final project for Northcoders Bootcamp, a guess who style alien game',
     href: '../projects/guess-what.html',
   },
   {
     title: 'What Carb?',
-    img: '../assets/project-images/what-carb-desktop-and-mobile.png',
+    img: '../assets/project-images-small/what-carb-small.png',
     about:
       'Recipe app for when you know what carbohydrate you want, but just not what to have it with',
     href: '../projects/what-carb.html',
   },
   {
     title: 'Portfolio 2023',
-    img: '../assets/project-images/old-portfolio-2023-desktop-mobile.png',
+    img: '../assets/project-images-small/old-portfolio-2023-small.png',
     about: 'My 2023 portfolio website',
     href: '../projects/portfolio-2023.html',
   },

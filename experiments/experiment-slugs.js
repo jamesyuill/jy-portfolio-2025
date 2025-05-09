be0f5ed9c7844b6f3e2bb84c60969e26fa2ea23b
James Yuill <jamesyuill@macbookpro.lan>
2025-05-09 18:12:40 +0100

    updates with a little logo for the programming language

--- experiments/experiment-slugs.js
+++ experiments/experiment-slugs.js
@@ -4,12 +4,14 @@ const experimentSlugs = [
     img: '../assets/experiment-images-small/youtube-audio-small.png',
     about: 'Scrapes, converts and slices Youtube audio based on a search query',
     href: '../experiments/youtube-audio.html',
+    language: 'P',
   },
   {
     title: 'Transparent Browser Window',
     img: '../assets/experiment-images-small/transparent-browser-small.png',
     about: 'Idea to make a browser that was transparent',
     href: '../experiments/transparent-browser.html',
+    language: 'JS',
   },
   {
     title: 'Browser Battleships',
@@ -17,6 +19,7 @@ const experimentSlugs = [
     about:
       'A game of battleships, but you have to place little browsers around the screen',
     href: '../experiments/browser-battleships.html',
+    language: 'JS',
   },
   {
     title: 'Browser Pong',
@@ -24,6 +27,7 @@ const experimentSlugs = [
     about:
       'A game of Pong, but using browser windows (I got very into messing about with the browser!)',
     href: '../experiments/browser-pong.html',
+    language: 'JS',
   },
   {
     title: 'Cli Art',
@@ -31,30 +35,35 @@ const experimentSlugs = [
     about:
       'Random CLI are generator, but in three languages, Javascript, C++ and Rust',
     href: '../experiments/cli-art.html',
+    language: '+',
   },
   {
     title: 'Audio Rendering',
     img: '../assets/experiment-images-small/audio-rendering-small.png',
     about: 'Unlock a website by singing a certain pitch',
     href: '../experiments/audio-rendering.html',
+    language: 'JS',
   },
   {
     title: 'Perfect Pitcher',
     img: '../assets/experiment-images-small/perfect-pitcher-small.png',
     about: 'Test your singing skills with my perfect pitch tester!',
     href: '../experiments/perfect-pitcher.html',
+    language: 'JS',
   },
   {
     title: 'Wiki Picture Scrapper',
     img: '../assets/experiment-images-small/wiki-webscraper-small.png',
     about: 'Webscrapper that grabs the pictures embedded in a Wiki page',
     href: '../experiments/wiki-scraper.html',
+    language: 'JS',
   },
   {
     title: '3d Snake',
     img: '../assets/experiment-images-small/3d-snake-small.png',
     about: 'Snake, but 3D!',
     href: '../experiments/3d-snake.html',
+    language: 'JS',
   },
 ];
 
--- projects/project-slugs.js
+++ projects/project-slugs.js
@@ -4,42 +4,49 @@ const projectSlugs = [
     img: '../assets/project-images-small/supplier-satellites-small.png',
     about: 'Custom Power BI visual for supplier management',
     href: '../projects/supplier-satellites.html',
+    language: 'JS',
   },
   {
     title: 'React Multiplayer Quiz Platform',
     img: '../assets/project-images-small/multiplayer-quiz-small.png',
     about: 'React Multiplayer quiz platform utilising socket.io',
     href: '../projects/multiplayer-quiz.html',
+    language: 'JS',
   },
   {
     title: 'Standup Pickeroo',
     img: '../assets/project-images-small/standup-pickeroo-small.png',
     about: 'Spin the Wheel style person picker for standups',
     href: '../projects/standup-pickeroo.html',
+    language: 'JS',
   },
   {
     title: 'Portfolio 2024',
     img: '../assets/project-images-small/jy-portfolio-2024-small.png',
     about: 'My 2024 portfolio website',
     href: '../projects/portfolio-2024.html',
+    language: 'JS',
   },
   {
     title: 'Project To-Do',
     img: '../assets/project-images-small/project-todo-small.png',
     about: 'A Kanban style app...maybe like Trello...shhh!',
     href: '../projects/project-todo.html',
+    language: 'TS',
   },
   {
     title: 'Balls Dropping',
     img: '../assets/project-images-small/balls-dropping-small.png',
     about: 'A synth where the notes are triggered by balls dropping',
     href: '../projects/balls-dropping.html',
+    language: 'JS',
   },
   {
     title: 'cable tv landing Page',
     img: '../assets/project-images-small/cable-tv-website-small.png',
     about: 'A simple landing page for a musical artist',
     href: '../projects/cable-tv.html',
+    language: 'JS',
   },
   {
     title: 'Etsy to Instagram Scraper',
@@ -47,6 +54,7 @@ const projectSlugs = [
     about:
       'Web scrapper that will grab contents of Etsy shop and selects random item to post about on Instagram',
     href: '../projects/etsy-to-insta.html',
+    language: 'JS',
   },
   {
     title: 'Guess What?!',
@@ -54,6 +62,7 @@ const projectSlugs = [
     about:
       'Final project for Northcoders Bootcamp, a guess who style alien game',
     href: '../projects/guess-what.html',
+    language: 'JS',
   },
   {
     title: 'What Carb?',
@@ -61,12 +70,14 @@ const projectSlugs = [
     about:
       'Recipe app for when you know what carbohydrate you want, but just not what to have it with',
     href: '../projects/what-carb.html',
+    language: 'JS',
   },
   {
     title: 'Portfolio 2023',
     img: '../assets/project-images-small/old-portfolio-2023-small.png',
     about: 'My 2023 portfolio website',
     href: '../projects/portfolio-2023.html',
+    language: 'JS',
   },
 ];
 
